a322f2ac4946877d401611b11f0790e9d57d42cb
TadXiao <zlxiao97@yahoo.com>
2023-03-07 17:54:54 +0800

    📦 NEW: 迁移第一个故事文档 (#6)
    Co-authored-by: 肖子立 <xiaozlb@digitalchina.com>

---
+++ docs/.vuepress/configs/navbar/zh.js
@@ -0,0 +1,19 @@
+export const navbarZh = [
+  {
+    text: 'React',
+    children: [
+      {
+        text: '云计算',
+        link: '/react/cloud/',
+      },
+      {
+        text: '物联网',
+        link: '/react/iot/',
+      },
+    ],
+  },
+  {
+    text: 'Vue.js',
+    children: [],
+  },
+];
---
+++ docs/.vuepress/configs/sidebar/zh.js
@@ -0,0 +1,19 @@
+export const sidebarZh = {
+  '/react': [
+    {
+      text: 'React',
+      children: [
+        {
+          text: '云计算',
+          link: '/react/cloud',
+          children: ['/react/cloud/MA001'],
+        },
+        {
+          text: '物联网',
+          link: '/react/iot',
+          children: [],
+        },
+      ],
+    },
+  ],
+};
